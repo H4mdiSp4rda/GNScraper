ef2899efdd48712517b2e9bdefc9a08e38a45e73
Sp4rda <Hamdi.Sparda@hotmail.com>
2023-09-21 22:24:16 +0100

    10 art scraped(no dup), db insert,query,purge work

--- README.md
+++ README.md
@@ -1,8 +1,81 @@
-<h1>GNews Scraper</h1>	
+# Business Intelligence and Deep Learning Project
 
-<h3>Part I:</h3>
-<p>Need to better handle and escape the 403 errors.<br>
-csv export needs work(skipping lines and row order.</p>
+## Project Overview
+This project aims to leverage Business Intelligence (BI) techniques and Deep Learning algorithms to scrap, analyze and classify news data into two categories: Fake and Real. The project is divided into several main parts for efficient development and execution.
 
-<h3>Part II:</h3>
-<p>scraping and storing targeted Markets/locations.</p>
+### I) Setting up the Environment
+The initial phase of the project involves setting up the development environment. This phase includes the following subtasks, which have already been completed:
+- Docker: Containerization of the project
+- GitHub: Version control for collaborative development
+- MongoDB: Data storage and management
+
+### II) Scraping Script
+In this phase, we will focus on developing a robust web scraping script to gather data for analysis. This phase includes the following tasks:
+
+1. **Data Scraping Optimization**:
+   - Implementation of various optimizations such as custom headers, user-agent spoofing, driver-agent, request throttling, and Tor proxy integration to avoid IP blocking and improve scraping efficiency.
+
+2. **Data Storage and Management in MongoDB**:
+   - **Data Insertion**: Already completed.
+   - **Data Update**: Already completed.
+   - **Duplicate Checking**: Already completed. Implement logic to identify and eliminate duplicate data entries.
+   - **Data Query**: Already completed. Create queries to retrieve relevant data from the MongoDB database.
+   - **Data Purging**: Already completed. Develop a mechanism to remove outdated or irrelevant data from the database.
+
+### III) Data Classification
+This phase focuses on using Deep Learning techniques to classify the scraped data into two categories: Fake and Real. This classification will help in identifying potentially misleading or inaccurate information. The specific tasks in this phase include:
+
+- Data Preprocessing: Prepare the scraped data for input into deep learning models. This may include text cleaning, feature extraction, and data transformation.
+- Model Development: Build and train deep learning models for the classification task. Experiment with various architectures, such as convolutional neural networks (CNNs) or recurrent neural networks (RNNs), and select the best-performing model.
+- Model Evaluation: Assess the performance of the classification models using relevant metrics (e.g., accuracy, precision, recall, F1-score).
+- Integration: Integrate the classification model into the project pipeline to automate the categorization of scraped data.
+
+### IV) Data Visualization with Django
+The final phase of the project involves creating a web-based data visualization interface using the Django framework. This interface will allow users to interactively explore the analyzed data. The tasks in this phase include:
+
+- Building a Django web application that connects to the MongoDB database.
+- Developing user-friendly dashboards and visualizations to present the results of the data classification.
+- Implementing features for user authentication and data access control.
+- Deploying the Django application to a web server for remote access.
+
+# Progress Checklist:
+
+### I) Setting up the Environment
+- Docker: Containerization of the project (Done) ✔
+- GitHub: Version control for collaborative development (Done) ✔
+- MongoDB: Data storage and management (Done) ✔
+
+### II) Scraping Script
+#### Data Scraping Optimization
+- Custom headers (Needs Work) ✘
+- User-agent spoofing (Needs Work) ✘
+- Request throttling (Needs Work) ✘
+- Tor proxy integration (Needs Work) ✘
+
+#### Data Storage and Management in MongoDB
+- Data Insertion (Done) ✔
+- Data Update (Done) ✔
+- Duplicate Checking (Done) ✔
+- Data Query (Done) ✔
+- Data Purging (Done) ✔
+
+### III) Data Classification (Fake/Real)
+- Data Preprocessing (Needs Work) ✘
+- Model Development (Needs Work) ✘
+- Model Evaluation (Needs Work) ✘
+- Integration (Needs Work) ✘
+
+### IV) Data Visualization with Django
+- Django Web Application (Needs Work) ✘
+- User-friendly Dashboards (Needs Work) ✘
+- User Authentication (Needs Work) ✘
+- Data Access Control (Needs Work) ✘
+- Deployment (Needs Work) ✘
+
+# Script Usage:
+- How to manage data in MongoDB:
+    - `--scrap`: Scrap data
+    - `--query`: Query the MongoDB collection
+    - `--purge`: Purge (clear) the MongoDB collection
+
+
--- scrap.py
+++ scrap.py
@@ -1,50 +1,49 @@
-import csv
+import argparse
 from newspaper import Article
 import pygooglenews
 import pymongo
 
 # Constants
 SEARCH_QUERY = "banking industry partnership news"
-NUM_ARTICLES_TO_SCRAPE = 10
+NUM_ARTICLES_TO_SCRAP = 10
 MONGODB_URL = "mongodb://172.17.0.2:27017/"
 DB_NAME = "gns_raw"
 COLLECTION_NAME = "articles"
 
-def scrape_articles():
+# Create a MongoDB client and select the database and collection
+def connect_to_mongodb():
+    client = pymongo.MongoClient(MONGODB_URL)
+    db = client[DB_NAME]
+    collection = db[COLLECTION_NAME]
+    return collection
+
+# Define the purge_db function
+def purge_db():
     try:
-        # Initialize Google News client
+        collection = connect_to_mongodb()
+        result = collection.delete_many({})
+        print(f"Purged {result.deleted_count} documents from the collection.")
+    except Exception as e:
+        print(f"An error occurred while purging the database: {e}")
+
+# Define the scrap_articles function
+def scrap_articles():
+    try:
         gn = pygooglenews.GoogleNews()
-
-        # Define your search query and get search results
         search_results = gn.search(SEARCH_QUERY)
-
-        # Create a list to store the extracted data
         data = []
 
-        # Iterate through the search results and scrape article content
-        for entry in search_results['entries']:
-            if len(data) >= NUM_ARTICLES_TO_SCRAPE:
-                break
-
+        for entry in search_results['entries'][:NUM_ARTICLES_TO_SCRAP]:
             try:
                 article = Article(entry['link'])
                 article.download()
                 article.parse()
-
-                # Extract relevant information from the article
-                title = article.title
-                source = entry['source']
-                published_time = entry['published']
-                article_url = entry['link']
-                content = article.text
-
-                # Append the data to the list
                 data.append({
-                    "Title": title,
-                    "Source": source,
-                    "Published Time": published_time,
-                    "Article URL": article_url,
-                    "Content": content
+                    "Title": article.title,
+                    "Source": entry['source'],
+                    "Published Time": entry['published'],
+                    "Article URL": entry['link'],
+                    "Content": article.text
                 })
             except Exception as e:
                 print(f"An error occurred while processing an article: {e}")
@@ -53,117 +52,88 @@ def scrape_articles():
     except Exception as e:
         print(f"An error occurred during scraping: {e}")
 
+# Define the insert_data_into_mongodb function
+# Define the insert_data_into_mongodb function
 def insert_data_into_mongodb(data):
     try:
-        # Establish a connection to the MongoDB server using a context manager
-        with pymongo.MongoClient(MONGODB_URL) as client:
-            # Select a database
-            db = client[DB_NAME]
+        collection = connect_to_mongodb()
+        inserted_count = 0  # Initialize the count of inserted documents
+        ignored_count = 0  # Initialize the count of ignored (duplicate) documents
 
-            # Create or select a collection
-            collection = db[COLLECTION_NAME]
-
-            # Use upsert to insert or update the data
-            for item in data:
-                # Define a filter to identify the document (assuming a unique key)
-                filter = {"Title": item["Title"]}  # You can change the key as needed
-
-                # Update or insert the document
-                collection.update_one(filter, {"$set": item}, upsert=True)
+        if data:
+            for article_data in data:
+                # Check if an article with the same URL already exists
+                existing_article = collection.find_one({"Article URL": article_data["Article URL"]})
+                if not existing_article:
+                    # If not found, insert the article
+                    collection.insert_one(article_data)
+                    inserted_count += 1  # Increment the count for each inserted document
+                else:
+                    ignored_count += 1  # Increment the count for each ignored (duplicate) document
+            
+            print(f"Inserted {inserted_count} unique documents into MongoDB.")
+            print(f"Ignored {ignored_count} duplicate documents.")
+        else:
+            print("No data to insert into MongoDB.")
     except Exception as e:
         print(f"An error occurred while inserting data into MongoDB: {e}")
 
-# def query_mongodb():
-#     try:
-#         # Establish a connection to the MongoDB server using a context manager
-#         with pymongo.MongoClient(MONGODB_URL) as client:
-#             # Select the database and collection
-#             db = client[DB_NAME]
-#             collection = db[COLLECTION_NAME]
-
-#             # Query and retrieve all documents (data) from the collection
-#             cursor = collection.find({})
-
-#             # Return the data as a list of dictionaries
-#             return list(cursor)
-#     except Exception as e:
-#         print(f"An error occurred while querying the database: {e}")
-
-
-
-# if __name__ == "__main__":
-#     scraped_data = scrape_articles()
-#     if scraped_data:
-#         insert_data_into_mongodb(scraped_data)
-
-#     retrieved_data = query_mongodb()
-
-#     # Print the retrieved data
-#     for item in retrieved_data:
-#         print(item)
-
+# Define the query_mongodb function
 def query_mongodb():
-    retrieved_data = []  # Initialize an empty list to store the data
     try:
-        # Establish a connection to the MongoDB server using a context manager
-        with pymongo.MongoClient(MONGODB_URL) as client:
-            # Select the database and collection
-            db = client[DB_NAME]
-            collection = db[COLLECTION_NAME]
+        collection = connect_to_mongodb()
+        cursor = collection.find()
 
-            # Query and retrieve all documents (data) from the collection
-            cursor = collection.find({})
+        count = 0  # Initialize a count variable
 
-            # Iterate through the cursor, print each document, and append it to retrieved_data
-            for document in cursor:
-                print("Title:", document.get("Title"))
-                # print("Source:", document.get("Source"))
-                # print("Published Time:", document.get("Published Time"))
-                # print("Article URL:", document.get("Article URL"))
-                # print("\n" + "=" * 50 + "\n")  # Separating lines for readability
+        for _ in cursor:
+            count += 1  # Increment the count for each document
 
-                # Append the document to the retrieved_data list
-                retrieved_data.append({
-                    "Title": document.get("Title"),
-                    "Source": document.get("Source"),
-                    "Published Time": document.get("Published Time"),
-                    "Article URL": document.get("Article URL")
-                })
+        cursor.rewind()  # Rewind the cursor to the beginning for printing
+
+        for document in cursor:
+            print("Title:", document.get("Title"))
+            print("Source:", document.get("Source"))
+            print("Published Time:", document.get("Published Time"))
+            print("Article URL:", document.get("Article URL"))
+            print("Content:")
+            # print(document.get("Content")) #uncomment if  u wanna check article text scraped
+            # print("\n" + "=" * 50 + "\n") ##uncomment if  u wanna check article text scraped
+
+        if count == 0:
+            print("No articles found in the MongoDB collection.")
+        else:
+            print(f"Number of articles found in the MongoDB collection: {count}")
 
     except Exception as e:
         print(f"An error occurred while querying the database: {e}")
-    
-    return retrieved_data  # Return the retrieved data as a list
 
+# Create a function to handle command-line arguments
+def main():
+    parser = argparse.ArgumentParser(description="Scrape and manage data in MongoDB")
+    parser.add_argument("--purge", action="store_true", help="Purge (clear) the MongoDB collection")
+    parser.add_argument("--scrap", action="store_true", help="Scrape data")
+    parser.add_argument("--query", action="store_true", help="Query the MongoDB collection")
+
+    args = parser.parse_args()
+    scraped_data = None  # Initialize scraped_data
+
+    if args.purge:
+        purge_db()
+
+    if args.scrap:
+        scraped_data = scrap_articles()
+        if scraped_data:
+            print(f"Scraped {len(scraped_data)} articles.")
+            insert_option = input("Do you want to store the scraped data in the database? (yes/no): ").strip().lower()
+            if insert_option == "yes":
+                insert_data_into_mongodb(scraped_data)
+            else:
+                print("Scraped data not stored in the database.")
+
+    if args.query:
+        query_mongodb()
 
 if __name__ == "__main__":
-    scraped_data = scrape_articles()
-    if scraped_data:
-        insert_data_into_mongodb(scraped_data)
+    main()
 
-    retrieved_data = query_mongodb()
-
-    # Check if retrieved_data is not None
-    if retrieved_data:
-        # Print the number of items found in the MongoDB collection
-        print(f"Number of items found in the MongoDB collection: {len(retrieved_data)}")
-    else:
-        print("No data found in the MongoDB collection.")
-
-def purge_db():
-    try:
-        # Establish a connection to the MongoDB server using a context manager
-        with pymongo.MongoClient(MONGODB_URL) as client:
-            # Select the database
-            db = client[DB_NAME]
-
-            # Select the collection
-            collection = db[COLLECTION_NAME]
-
-            # Delete all documents in the collection
-            result = collection.delete_many({})
-
-            # Print the number of documents deleted
-            print(f"Purged {result.deleted_count} documents from the collection.")
-    except Exception as e:
-        print(f"An error occurred while purging the database: {e}")
