93f73731db6ad941bdf3474d5142401ffab9656f
Sp4rda <Hamdi.Sparda@hotmail.com>
2023-12-20 18:40:48 +0100

    UPDATED logging (A new log file will be used each day)

--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,10 @@ from gevent import monkey
 monkey.patch_all(thread=False, select=False)
 
 import logging
-from logging.handlers import RotatingFileHandler
+from logging.handlers import TimedRotatingFileHandler
+from datetime import datetime
 
-
-def setup_logging(log_name, log_file_path):
+def setup_logging(log_name, base_log_file_name):
     # Get or create a logger
     logger = logging.getLogger(log_name)
     logger.setLevel(logging.DEBUG)
@@ -13,8 +13,13 @@ def setup_logging(log_name, log_file_path):
     # Create a log formatter
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
-    # Set up a rotating file handler
-    file_handler = RotatingFileHandler(log_file_path, maxBytes=10 * 1024 * 1024, backupCount=5)
+    # Generate log file name with current date
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    log_file_name = f"{base_log_file_name}-{current_date}.log"
+    log_file_path = f"./logs/{log_file_name}"
+
+    # Set up a timed rotating file handler
+    file_handler = TimedRotatingFileHandler(log_file_path, when='midnight', interval=1, backupCount=5)
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(formatter)
 
